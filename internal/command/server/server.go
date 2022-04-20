925a3cc9478fd04f056260340ab354d4f3de19ff
takumin <takumiiinn@gmail.com>
2022-04-21 02:58:00 +0900

    refactor: move server package

--- internal/command/server/server.go
+++ internal/command/server/server.go
@@ -1,14 +1,11 @@
 package server
 
 import (
-	"context"
-	"fmt"
 	"net"
 	"net/http"
 	"os"
 	"strings"
 
-	"github.com/go-piv/piv-go/piv"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"github.com/urfave/cli/v2"
 	"golang.org/x/net/http2"
@@ -18,6 +15,7 @@ import (
 
 	"github.com/takumin/ykmgr/internal/config"
 	"github.com/takumin/ykmgr/internal/resolver"
+	"github.com/takumin/ykmgr/internal/server"
 	"github.com/takumin/ykmgr/pkg/yubikey/v1"
 )
 
@@ -53,7 +51,7 @@ func httpGrpcRouter(grpcServer *grpc.Server, httpHandler http.Handler) http.Hand
 
 func action(c *config.Config) func(ctx *cli.Context) error {
 	return func(ctx *cli.Context) error {
-		ykServer, err := NewServer()
+		ykServer, err := server.NewServer()
 		if err != nil {
 			return err
 		}
@@ -98,79 +96,4 @@ func action(c *config.Config) func(ctx *cli.Context) error {
 			&http2.Server{},
 		))
 	}
-}
-
-type server struct {
-	yubikey.UnimplementedYubikeyServiceServer
-
-	yk map[uint32]*piv.YubiKey
-}
-
-func NewServer() (*server, error) {
-	cards, err := piv.Cards()
-	if err != nil {
-		return nil, err
-	}
-
-	yubikeys := make(map[uint32]*piv.YubiKey, len(cards))
-	for _, v := range cards {
-		handle, err := piv.Open(v)
-		if err != nil {
-			return nil, err
-		}
-
-		serial, err := handle.Serial()
-		if err != nil {
-			return nil, err
-		}
-
-		yubikeys[serial] = handle
-	}
-
-	return &server{
-		yk: yubikeys,
-	}, nil
-}
-
-func (s *server) Close() map[uint32]error {
-	err := make(map[uint32]error, len(s.yk))
-	for k, v := range s.yk {
-		err[k] = v.Close()
-	}
-	return err
-}
-
-func (s *server) GetSerials(ctx context.Context, req *yubikey.GetSerialsRequest) (*yubikey.GetSerialsResponse, error) {
-	keys := make([]uint32, 0, len(s.yk))
-	for k := range s.yk {
-		keys = append(keys, k)
-	}
-	return &yubikey.GetSerialsResponse{
-		Serials: keys,
-	}, nil
-}
-
-func (s *server) GetVersion(ctx context.Context, req *yubikey.GetVersionRequest) (*yubikey.GetVersionResponse, error) {
-	var version yubikey.Version
-	if _, ok := s.yk[req.GetSerial()]; ok {
-		version.Major = uint32(s.yk[req.GetSerial()].Version().Major)
-		version.Minor = uint32(s.yk[req.GetSerial()].Version().Minor)
-		version.Patch = uint32(s.yk[req.GetSerial()].Version().Patch)
-	}
-	return &yubikey.GetVersionResponse{
-		Version: &version,
-	}, nil
-}
-
-func (s *server) GetRetries(ctx context.Context, req *yubikey.GetRetriesRequest) (*yubikey.GetRetriesResponse, error) {
-	if _, ok := s.yk[req.GetSerial()]; !ok {
-		return nil, fmt.Errorf("not found serial")
-	}
-	retries, err := s.yk[req.GetSerial()].Retries()
-	if err != nil {
-		return nil, err
-	}
-	return &yubikey.GetRetriesResponse{
-		Retries: uint32(retries),
-	}, nil
 }
---
+++ internal/server/server.go
@@ -0,0 +1,84 @@
+package server
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/go-piv/piv-go/piv"
+	"github.com/takumin/ykmgr/pkg/yubikey/v1"
+)
+
+type server struct {
+	yubikey.UnimplementedYubikeyServiceServer
+
+	yk map[uint32]*piv.YubiKey
+}
+
+func NewServer() (*server, error) {
+	cards, err := piv.Cards()
+	if err != nil {
+		return nil, err
+	}
+
+	yubikeys := make(map[uint32]*piv.YubiKey, len(cards))
+	for _, v := range cards {
+		handle, err := piv.Open(v)
+		if err != nil {
+			return nil, err
+		}
+
+		serial, err := handle.Serial()
+		if err != nil {
+			return nil, err
+		}
+
+		yubikeys[serial] = handle
+	}
+
+	return &server{
+		yk: yubikeys,
+	}, nil
+}
+
+func (s *server) Close() map[uint32]error {
+	err := make(map[uint32]error, len(s.yk))
+	for k, v := range s.yk {
+		err[k] = v.Close()
+	}
+	return err
+}
+
+func (s *server) GetSerials(ctx context.Context, req *yubikey.GetSerialsRequest) (*yubikey.GetSerialsResponse, error) {
+	keys := make([]uint32, 0, len(s.yk))
+	for k := range s.yk {
+		keys = append(keys, k)
+	}
+	return &yubikey.GetSerialsResponse{
+		Serials: keys,
+	}, nil
+}
+
+func (s *server) GetVersion(ctx context.Context, req *yubikey.GetVersionRequest) (*yubikey.GetVersionResponse, error) {
+	var version yubikey.Version
+	if _, ok := s.yk[req.GetSerial()]; ok {
+		version.Major = uint32(s.yk[req.GetSerial()].Version().Major)
+		version.Minor = uint32(s.yk[req.GetSerial()].Version().Minor)
+		version.Patch = uint32(s.yk[req.GetSerial()].Version().Patch)
+	}
+	return &yubikey.GetVersionResponse{
+		Version: &version,
+	}, nil
+}
+
+func (s *server) GetRetries(ctx context.Context, req *yubikey.GetRetriesRequest) (*yubikey.GetRetriesResponse, error) {
+	if _, ok := s.yk[req.GetSerial()]; !ok {
+		return nil, fmt.Errorf("not found serial")
+	}
+	retries, err := s.yk[req.GetSerial()].Retries()
+	if err != nil {
+		return nil, err
+	}
+	return &yubikey.GetRetriesResponse{
+		Retries: uint32(retries),
+	}, nil
+}
